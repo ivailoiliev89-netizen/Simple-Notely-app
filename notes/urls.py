778ab98293a7b367cbd4070a0c55838372b0dd51
Ivailo Iliqnov Iliev <ivailoiliev89@gmail.com>
2026-01-22 10:27:23 +0200

    Update URL patterns for note views

--- notes/urls.py
+++ notes/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.NoteListView.as_view(), name='note_List'),
-    path('create/', views.NoteListView.as_view(), name='note-create'),
-    path('<int:pk>/', views.NoteListView.as_view(), name='note-detail'),
-    path('<int:pk>/edit/', views.NoteListView.as_view(), name='note-edit'),
-    path('int:pk>/delete/', views.NoteListView.as_view(), name='note-delete'),
+    path('', views.NoteListView.as_view(), name='note-list'),
+    path('create/', views.NoteCreateView.as_view(), name='note-create'),
+    path('<int:pk>/', views.NoteDetailView.as_view(), name='note-detail'),
+    path('<int:pk>/edit/', views.NoteUpdateView.as_view(), name='note-edit'),
+    path('<int:pk>/delete/', views.NoteDeleteView.as_view(), name='note-delete'),
 ]
